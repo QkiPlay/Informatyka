116a2161254b861b4be3e44fd324523e290b9509
Grzegorz Faszcza <32841516+QkiPlay@users.noreply.github.com>
2023-12-13 00:00:35 +0100

    Create Określenie największej liczby o parzystym indeksie.py

---
+++ Określenie największej liczby o parzystym indeksie.py
@@ -0,0 +1,14 @@
+import random
+wszystkie=[]
+for i in range(100):
+    wszystkie.append(random.randint(1,10000))
+
+Najwieksza_liczba_p = min(wszystkie)-1
+IndeksNieParzystyNajwiekszej = -1
+
+for i in range(len(wszystkie)):
+    if i % 2 == 0:
+        if(Najwieksza_liczba_p < wszystkie[i]):
+            Najwieksza_liczba_p = wszystkie[i]
+            IndeksNieParzystyNajwiekszej = i 
+print('Największa liczba o Parzystym indeksie to', Najwieksza_liczba_p, 'z indeksem numer', IndeksNieParzystyNajwiekszej)
